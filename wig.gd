e942fa753b38413906299c57945bebc0218a58f4
MaoZ3 <117559028+MaoZ3@users.noreply.github.com>
2024-05-12 23:30:12 -0300

    upup

--- wig.gd
+++ wig.gd
@@ -5,11 +5,23 @@ extends CharacterBody2D
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
+	
+	if input_direction.x != 0:
+		
+		""" """
+		
+		$Sprite2D.flip_h = true
+	elif input_direction.x > 0:
+		$Sprite2D.flip_v = false
 
-func _physics_process(delta):
-	get_input()
+
+
+func _physics_process(_delta):
+	var input_direction = get_input()
+	
+	
 	move_and_slide()
 
 
-func _on_node_2d_area_entered(area):
+func _on_node_2d_area_entered(_area):
 	pass # Replace with function body.
