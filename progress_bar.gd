5173bb57fe398b06dcb321550137984ea0874641
MaoZ3 <117559028+MaoZ3@users.noreply.github.com>
2024-05-25 01:05:00 -0300

    upup

--- progress_bar.gd
+++ progress_bar.gd
@@ -2,18 +2,16 @@ extends ProgressBar
 
 @export var motomami = preload("res://motomami.tscn")
 
-
 func update():
 	pass # Called when the node enters the scene tree for the first time.
 	
 
-	
-	
-	
 func _ready():
 	#motomami.connect("estMotocolision", Callable(motomami, "_on_est_Motocolision"))
-	
 	pass
+	
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
