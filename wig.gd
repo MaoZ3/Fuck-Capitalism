e7769a392c0b448e724220d5124462584126e118
MaoZ3 <117559028+MaoZ3@users.noreply.github.com>
2024-05-30 20:05:00 -0300

    intro vuela peluca

--- wig.gd
+++ wig.gd
@@ -6,47 +6,47 @@ var screen_size
 
 
 func _ready():
-    screen_size = get_viewport_rect().size
-    connect("character_moved",Callable( self, "_on_character_moved"))	
-    
+	screen_size = get_viewport_rect().size
+	connect("character_moved",Callable( self, "_on_character_moved"))	
+	
 func _input(_event):
-    if Input.is_action_pressed("ClickmotoMami"):
-        $AudioStreamPlayer.play()
-        print("clickPress")
-    
-    elif Input.is_action_just_released("ClickmotoMami"):
-        $AudioStreamPlayer.stop()
-        $AudioStreamPlayer2.play()
-        print("clickRelease")
-    
+	if Input.is_action_pressed("ClickmotoMami"):
+		$AudioStreamPlayer.play()
+		print("clickPress")
+	
+	elif Input.is_action_just_released("ClickmotoMami"):
+		$AudioStreamPlayer.stop()
+		$AudioStreamPlayer2.play()
+		print("clickRelease")
+	
 
 func get_input():
-    var input_direction = Input.get_vector("left", "right", "up", "down")
-    velocity = input_direction * speed
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
 
-    if velocity.x != 0:
-        $Sprite2D.flip_v = false
-        $Sprite2D.flip_h = velocity.x < 0
+	if velocity.x != 0:
+		$Sprite2D.flip_v = false
+		$Sprite2D.flip_h = velocity.x < 0
 
-    elif velocity.y != 0:
-        $Sprite2D.flip_v = velocity.y > 0
-    
+	elif velocity.y != 0:
+		$Sprite2D.flip_v = velocity.y > 0
+	
 
 func start(position):
-    self.position = position
-    
+	self.position = position
+	
 
 func new_game():
-    # Asegúrate de que el nodo 'Wig' tenga un script que defina la función 'start'.
-    $Wig.start($StartPosition.position)
-    $StartTimer.start()
-    
+	# Asegúrate de que el nodo 'Wig' tenga un script que defina la función 'start'.
+	$Wig.start($StartPosition.position)
+	$StartTimer.start()
+	
 
 func _physics_process(_delta):
-    position = position.clamp(Vector2.ZERO, screen_size)
-    var input_direction = get_input()
-    move_and_slide()
-    
+	position = position.clamp(Vector2.ZERO, screen_size)
+	var input_direction = get_input()
+	move_and_slide()
+	
 
 
 
